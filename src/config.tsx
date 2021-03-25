aaa4f44631f0dc4f664e26f42691cd5678b04aa2
Yurii Oliiarnyk <oliiarnyk14@gmail.com>
2021-03-25 15:22:36 +0200

    Fixed config platform_protocol

--- src/config.tsx
+++ src/config.tsx
@@ -3,7 +3,7 @@ export const DEBUG = process.env.NODE_ENV === 'development';
 export const TOKEN_KEY = 'authenticationToken';
 export const DOMAIN_KEY = 'domain';
 
-export const PLATFORM_PROTOCOL = 'https://';
+export const PLATFORM_PROTOCOL = !DEBUG ? 'https://' : '';
 export const PLATFORM_SUFFIX = !DEBUG ? '.fieldbi.io' : '';
 
 export const getAPI = (value: string): string => {
