70714d5f3bf38d8d8e99076f3bbc98feb207d699
Yurii Oliiarnyk <oliiarnyk14@gmail.com>
2021-05-27 10:58:46 +0300

    Migrations settings

--- agribi-app/metro.config.js
+++ agribi-app/metro.config.js
@@ -1,23 +1,58 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const path = require('path');
+const blacklist = require('metro-config/src/defaults/blacklist');
+const escape = require('escape-string-regexp');
+const pak = require('../package.json');
+
+const root = path.resolve(__dirname, '..');
+
+const modules = [...Object.keys(pak.peerDependencies)];
 
 const { getDefaultConfig } = require('metro-config');
 
 module.exports = (async () => {
+  const config = await getDefaultConfig();
+
   const {
     resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig();
+  } = config;
+
   return {
-    transformer: {
-      babelTransformerPath: require.resolve('react-native-svg-transformer'),
-    },
+    projectRoot: __dirname,
+    watchFolders: [root],
     resolver: {
+      blacklistRE: blacklist(
+        modules.map(m => new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`))
+      ),
+
+      extraNodeModules: modules.reduce((acc, name) => {
+        acc[name] = path.join(__dirname, 'node_modules', name);
+        return acc;
+      }, {}),
       assetExts: assetExts.filter(ext => ext !== 'svg'),
       sourceExts: [...sourceExts, 'svg'],
     },
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+    },
+    // plugins: [
+    //   [
+    //     'module-resolver',
+    //     {
+    //       root: ['../'],
+    //       extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+    //       alias: {
+    //         'agro-package': ['../src/'],
+    //       },
+    //     },
+    //   ],
+    // ],
   };
 })();
